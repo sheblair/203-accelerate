c79d3e374df9578ad6aa480b6df58fa7fbc95964
sheblair <66489366+sheblair@users.noreply.github.com>
2023-04-19 10:55:42 -0400

    added contact form

---
+++ wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-681e8d46fd2c0890d2dc14225a7bbac5e4a67311',
+        'version' => 'dev-681e8d46fd2c0890d2dc14225a7bbac5e4a67311',
+        'reference' => '681e8d46fd2c0890d2dc14225a7bbac5e4a67311',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-681e8d46fd2c0890d2dc14225a7bbac5e4a67311',
+            'version' => 'dev-681e8d46fd2c0890d2dc14225a7bbac5e4a67311',
+            'reference' => '681e8d46fd2c0890d2dc14225a7bbac5e4a67311',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
