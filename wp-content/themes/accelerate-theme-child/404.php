0582e76edb1fab98463b094b60548ee4d741758a
sheblair <66489366+sheblair@users.noreply.github.com>
2023-04-25 11:06:10 -0400

    started 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the 404 not found page
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+            <div class="404-container">
+                <img class="404-image" src="" alt="" />
+                <h1>Whoops, Took a Wrong Turn...</h1>
+                <p>Sorry, this page no longer exists, never existed or has been
+                    moved. We feel like complete jerks for totally misleading you.</p>
+                <p>Feel free to take a look around our <a href="http://localhost:8888/accelerate/blog/">blog</a> or some of
+                    our featured <a href="http://localhost:8888/accelerate/case-studies/">work</a>.</p>
+            </div>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
