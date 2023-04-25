b443a4e5bd63d37028bec00e863ee34e73351099
sheblair <66489366+sheblair@users.noreply.github.com>
2023-04-25 10:59:14 -0400

    about page styles complete

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -35,59 +35,68 @@ get_header(); ?>
                 $contact_button_url = get_field('contact_button_url');
             ?>
 
-            <div class="about-top-section">
+            <div class="about-heading">
                 <?php the_content(); ?>
-
-                <div class="section-1">
-                    <h3><?php if($section_1_heading) {
-                        echo $section_1_heading;
-                    } ?></h3>
-                    <p><?php if($section_1_paragraph) {
-                        echo $section_1_paragraph;
-                    } ?></p>
-                    <div class="section-1-image">
+            </div>
+            <div class="about-main">
+                <div id="about-section-1" class="about-section">
+                    <div class="section-image">
                         <?php if($section_1_image) {
                             echo wp_get_attachment_image( $section_1_image, $size );
                         } ?>
                     </div>
+                    <div class="about-section-text">
+                        <h3><?php if($section_1_heading) {
+                            echo $section_1_heading;
+                        } ?></h3>
+                        <p><?php if($section_1_paragraph) {
+                            echo $section_1_paragraph;
+                        } ?></p>
+                    </div>
                 </div>
 
-                <div class="section-2">
-                    <h3><?php if($section_2_heading) {
-                        echo $section_2_heading;
-                    } ?></h3>
-                    <p><?php if($section_2_paragraph) {
-                        echo $section_2_paragraph;
-                    } ?></p>
-                    <div class="section-2-image">
+                <div id="about-section-2" class="about-section">
+                    <div class="about-section-text">
+                        <h3><?php if($section_2_heading) {
+                            echo $section_2_heading;
+                        } ?></h3>
+                        <p><?php if($section_2_paragraph) {
+                            echo $section_2_paragraph;
+                        } ?></p>
+                    </div>
+                    <div class="section-image">
                         <?php if($section_2_image) {
                             echo wp_get_attachment_image( $section_2_image, $size );
                         } ?>
                     </div>
                 </div>
 
-                <div class="section-3">
-                    <h3><?php if($section_3_heading) {
-                        echo $section_3_heading;
-                    } ?></h3>
-                    <p><?php if($section_3_paragraph) {
-                        echo $section_3_paragraph;
-                    } ?></p>
-                    <div class="section-3-image">
+                <div id="about-section-3" class="about-section">
+                    <div class="section-image">
                         <?php if($section_3_image) {
                             echo wp_get_attachment_image( $section_3_image, $size );
                         } ?>
                     </div>
+                    <div class="about-section-text">
+                        <h3><?php if($section_3_heading) {
+                            echo $section_3_heading;
+                        } ?></h3>
+                        <p><?php if($section_3_paragraph) {
+                            echo $section_3_paragraph;
+                        } ?></p>
+                    </div>
                 </div>
 
-                <div class="section-4">
-                    <h3><?php if($section_4_heading) {
-                        echo $section_4_heading;
-                    } ?></h3>
-                    <p><?php if($section_4_paragraph) {
-                        echo $section_4_paragraph;
-                    } ?></p>
-                    <div class="section-4-image">
+                <div id="about-section-4" class="about-section">
+                    <div class="about-section-text">
+                        <h3><?php if($section_4_heading) {
+                            echo $section_4_heading;
+                        } ?></h3>
+                        <p><?php if($section_4_paragraph) {
+                            echo $section_4_paragraph;
+                        } ?></p>
+                    </div>
+                    <div class="section-image">
                         <?php if($section_4_image) {
                             echo wp_get_attachment_image( $section_4_image, $size );
                         } ?>
@@ -95,7 +104,7 @@ get_header(); ?>
                 </div>
             </div>
             
-            <div class="about-bottom-section">
+            <div class="about-bottom">
                 <h2><?php if($bottom_heading) { echo $bottom_heading; } ?></h2>
                 <a href="<?php echo $contact_button_url; ?>"><button><?php echo $contact_button_label ?></button></a>
             </div>
