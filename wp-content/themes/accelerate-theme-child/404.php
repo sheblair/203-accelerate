bf9e7f0d0ede24e5e97275f6da39ef12dfc7d189
sheblair <66489366+sheblair@users.noreply.github.com>
2023-04-26 12:01:15 -0400

    added 404 page

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -11,14 +11,20 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-            <div class="404-container">
-                <img class="404-image" src="" alt="" />
-                <h1>Whoops, Took a Wrong Turn...</h1>
-                <p>Sorry, this page no longer exists, never existed or has been
-                    moved. We feel like complete jerks for totally misleading you.</p>
-                <p>Feel free to take a look around our <a href="http://localhost:8888/accelerate/blog/">blog</a> or some of
-                    our featured <a href="http://localhost:8888/accelerate/case-studies/">work</a>.</p>
+		<div id="not-found-main" class="main-content" role="main">
+            <div class="not-found-container">
+                <?php 
+                    $url_to_my_attachment = "http://localhost:8888/accelerate/wp-content/uploads/2023/04/map.png";
+                    $attachment_id = attachment_url_to_postid($url_to_my_attachment);
+
+                echo wp_get_attachment_image($attachment_id); ?>
+                <div class="not-found-text">
+                    <h1>Whoops, Took a Wrong Turn...</h1>
+                    <p>Sorry, this page no longer exists, never existed or has been
+                        moved. We feel like complete jerks for totally misleading you.</p>
+                    <p>Feel free to take a look around our <a href="http://localhost:8888/accelerate/blog/">blog</a> or some of
+                        our featured <a href="http://localhost:8888/accelerate/case-studies/">work</a>.</p>
+                </div>       
             </div>
 		</div><!-- .main-content -->
 
